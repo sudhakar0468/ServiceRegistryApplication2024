a5feebc56f33bb010f7016671647a3af0804c2c8
sudhakar0468 <pogula.sudhakarreddy@gmail.com>
2024-06-28 11:15:59 +0530

    service registry configuration in eureka server

--- src/main/java/com/example/sample/ServiceRegistryApplication2024/ServiceRegistryApplication2024Application.java
+++ src/main/java/com/example/sample/ServiceRegistryApplication2024/ServiceRegistryApplication2024Application.java
@@ -2,8 +2,10 @@ package com.example.sample.ServiceRegistryApplication2024;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication2024Application {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/sample/ServiceRegistryApplication2024/ServiceRegistryApplication2024ApplicationTests.java
+++ src/test/java/com/example/sample/ServiceRegistryApplication2024/ServiceRegistryApplication2024ApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.sample.ServiceRegistryApplication2024;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ServiceRegistryApplication2024ApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
