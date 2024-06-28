6516628e94410388a42197d765292b7cfd292dde
sudhakar0468 <pogula.sudhakarreddy@gmail.com>
2024-06-28 10:37:56 +0530

    git initial commit in servicerepository project

---
+++ src/main/java/com/example/sample/ServiceRegistryApplication2024/ServiceRegistryApplication2024Application.java
@@ -0,0 +1,13 @@
+package com.example.sample.ServiceRegistryApplication2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryApplication2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sample/ServiceRegistryApplication2024/ServiceRegistryApplication2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample.ServiceRegistryApplication2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplication2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
